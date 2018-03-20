bead0fb1f07d2f5ea23717ccb2951af3e00953b4
Lauren Gray <lauren98gray@gmail.com>
2018-03-19 23:30:30 -0500

    Store lines from "curriculum.dat" as separate elements in an ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         System.out.println("\n\nHello, RegistrationApp!\n");
+
+        //Read and store the data from "curriculum.dat"
+        ArrayList<String> curriculumLines = createListFromFileLines("curriculum.dat");
+        System.out.println(curriculumLines);
+
+
     }
+
+    private static ArrayList<String> createListFromFileLines(String fileName) throws IOException {
+        ArrayList<String> lines = new ArrayList<String>();
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+        for (String line = in.readLine(); line != null; line = in.readLine()) {
+            lines.add(line);
+        }
+        return lines;
+    }
+
+
+
 }
