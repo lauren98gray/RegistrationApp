def9d1e2f04a16b71dba5da70dfbc49abfdf8283
Lauren Gray <lauren98gray@gmail.com>
2018-03-20 20:13:01 -0500

    Output the data and calculate the total number of hours in the curriculum

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,46 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("\n\nHello, RegistrationApp!\n");
+        System.out.println("\n\nHello, RegistrationApp!\n\n");
 
         //Read and store the data from "curriculum.dat"
         ArrayList<String> curriculumLines = createListFromFileLines("curriculum.dat");
-        System.out.println(curriculumLines);
+
+        //Output the data
+        String outputCurriculum = formatCurriculumForOutput(curriculumLines);
+        System.out.println("The curriculum is as follows:\n\n" + outputCurriculum);
+
+        //Calculate the total hours in the curriculum
+        int totalCurriculumHours = calculateTotalCurriculumHours(curriculumLines);
+        System.out.println("The total number of hours in the curriculum is " + totalCurriculumHours);
 
 
+
+    }
+
+    private static String formatCurriculumForOutput(ArrayList<String> curriculumLines) {
+        String output = "";
+        for (String line : curriculumLines){
+            output += line+ "\n";
+        }
+        return output;
+    }
+
+    private static ArrayList<Integer> getCreditHours(ArrayList<String> curriculumLines) {
+        ArrayList<Integer> hours = new ArrayList<Integer>();
+        for (String course : curriculumLines){
+                hours.add(Integer.valueOf(course.substring(course.length()-1, course.length())));
+        }
+        return hours;
+    }
+
+    private static int calculateTotalCurriculumHours(ArrayList<String> curriculumLines) {
+        ArrayList<Integer> hours = getCreditHours(curriculumLines);
+        int sum = 0;
+        for (int hour : hours) {
+            sum += hour;
+        }
+        return sum;
     }
 
     private static ArrayList<String> createListFromFileLines(String fileName) throws IOException {
